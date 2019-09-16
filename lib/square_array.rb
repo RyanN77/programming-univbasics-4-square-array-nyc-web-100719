b4ce58dd3758ca5a8da827f3c95941769c1f11e7
adventurous-class-0665 <rngao97@gmail.com>
2019-09-16 20:25:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   number = 0 
   new_array = []
   while number < array.length do
-  new_array = array[number]**2
+  new_array[number] = array[number]**2
   number += 1
   end
 return new_array
